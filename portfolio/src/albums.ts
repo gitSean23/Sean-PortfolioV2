c90fe699405301a678862357d65aa8b77c8972a3
Sean <sean23@gmail.com>
2024-01-19 16:40:30 -0500

    layout + buttons

---
+++ portfolio/src/albums.ts
@@ -0,0 +1,21 @@
+export interface Album {
+    name: string,
+    cover: string
+}
+
+export const myAlbums: Album[] = [
+    {
+        name: "Experience",
+        cover: ""
+    },
+
+    {
+        name: "Projects",
+        cover: ""
+    },
+
+    {
+        name: "Contact",
+        cover: ""
+    }
+]
