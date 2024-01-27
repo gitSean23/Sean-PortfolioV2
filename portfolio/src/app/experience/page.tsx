ef2a1ff810da73a4d93cc9be365b9942a1cf8cc2
Sean <sean23@gmail.com>
2024-01-27 02:01:38 -0500

    Added content to the experience page and fixed bg color issue

--- portfolio/src/app/experience/page.tsx
+++ portfolio/src/app/experience/page.tsx
@@ -1,7 +1,38 @@
+import { myAlbums } from "@/albums";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Experience() {
   return (
-    <div>
-      <h1>Experience</h1>
+    <div className=" bg-black overflow-hidden flex flex-col min-h-screen">
+      <div className=" inline-flex relative top-8 mx-8 ">
+        <Image alt={myAlbums[0].name} src={myAlbums[0].cover}></Image>
+        <h3 className="relative top-48 mx-5">Sean Dev</h3>
+        <h3 className="relative top-48">|</h3>
+        <h3 className="relative top-48 mx-5">Experience</h3>
+      </div>
+      <div className="mx-20 relative top-24 bg-black">
+        <div className=" inline-flex gap-96">
+          <Link href="https://github.com/gitSean23?tab=repositories">NASA</Link>
+          <p>Software Engineer Intern</p>
+        </div>
+        <br></br>
+        <br></br>
+        <div className=" inline-flex gap-80">
+          <Link href="https://github.com/gitSean23?tab=repositories">
+            Knight Hacks
+          </Link>
+          <p className=" relative left-2">Software Engineer</p>
+        </div>
+        <br></br>
+        <br></br>
+        <div className=" inline-flex gap-80">
+          <Link href="https://github.com/gitSean23?tab=repositories">
+            Wear-Lab
+          </Link>
+          <p className=" relative left-9">Software Engineer</p>
+        </div>
+      </div>
     </div>
   );
 }
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden flex flex-col min-h-screen bg-black">
       <h1 className="mx-2 text-4xl">Sean Dev</h1>
       <Button variant="link" className="w-full justify-start mx-2">
         Neon
@@ -26,11 +26,14 @@ export default function Home() {
       <div className="flex flex-wrap mx-2 gap-20 p-9">
         {myAlbums.map((data, key) => (
           <Link key={key} href={data.url}>
-            <Image alt="yo" src={data.cover} placeholder="blur"></Image>
+            <Image alt={data.name} src={data.cover} placeholder="blur"></Image>
             <p className="relative top-3">{data.name}</p>
           </Link>
         ))}
       </div>
+      <br></br>
+      <br></br>
+      <h1 className="mx-10 text-4xl">About Me</h1>
     </div>
   );
 }
