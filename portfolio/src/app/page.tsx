14644fc5a22c942f885b1b53ee3a2d17808bec36
gitSean23 <the.feldman23@gmail.com>
2024-03-31 18:09:38 -0400

    Update metadata in layout.tsx and remove unused imports in page.tsx; added new tab icon

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
 import { Home as HomePage } from "../components/home";
 
 export default function Home() {
